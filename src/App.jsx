2d21ec47825f5b20bfe3f515991a029635aad637
Vidyesh Churi <vidyesh95@gmail.com>
2024-12-06 20:42:21 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div className="w-20 h-10 bg-amber-50">App</div>
+    )
 }
 
 export default App

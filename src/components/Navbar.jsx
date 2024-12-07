d74fa0ef44916974ab5cae833b25761ca6802424
Vidyesh Churi <vidyesh95@gmail.com>
2024-12-07 10:18:53 +0530

    background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import {assets} from "../assets/assets.js";
+import {useNavigate} from "react-router-dom";
 
 const Navbar = () => {
+    const navigate = useNavigate()
     return (
         <>
             <div className="w-full flex justify-between items-center font-semibold">
                 <div className="flex items-center gap-2">
-                    <img className="w-8 bg-black p-2 rounded-2xl cursor-pointer" src={assets.arrow_left} alt="back"/>
-                    <img className="w-8 bg-black p-2 rounded-2xl cursor-pointer" src={assets.arrow_right} alt="next"/>
+                    <img onClick={() => navigate(-1)}
+                         className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+                         src={assets.arrow_left} alt="back"/>
+                    <img onClick={() => navigate(1)}
+                         className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+                         src={assets.arrow_right} alt="next"/>
                 </div>
                 <div className="flex items-center gap-4">
                     <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
